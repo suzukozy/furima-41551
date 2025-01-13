88b24910b7cf1a036bff09dc7e70bb6b47707b45
suzukozy <suzukozy0405@yahoo.co.jp>
2025-01-13 21:29:35 +0900

    レビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.includes(:order).order(created_at: :desc)
   end
 
   def new
