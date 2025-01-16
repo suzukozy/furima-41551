50957cb9f44fc985c7c0c83ba9d6b770cb28634b
suzukozy <suzukozy0405@yahoo.co.jp>
2025-01-16 21:37:47 +0900

    Revert "商品情報編集機能"
    This reverts commit d6232d0828a966caab8df705e25b026da57ddb49.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.order(created_at: :desc)
+    @items = Item.includes(:order).order(created_at: :desc)
   end
 
   def new
@@ -20,23 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-    # if @item.order.present?
-    #  redirect_to root_path
-    # elsif current_user.id != @item.user_id
-    return unless current_user.id != @item.user_id
-
-    redirect_to new_user_session_path
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit, status: :unprocessable_entity
-    end
   end
 
   private
