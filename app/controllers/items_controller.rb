774f719aa1315b39560ac17ca897c088e696d0d2
suzukozy <suzukozy0405@yahoo.co.jp>
2025-01-16 19:37:30 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    # if @item.order.present?
+    #  redirect_to root_path
+    # elsif current_user.id != @item.user_id
     return unless current_user.id != @item.user_id
 
     redirect_to new_user_session_path
