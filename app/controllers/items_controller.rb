57b9c355e05d223417905bb96fa08b1395d24ee5
suzukozy <suzukozy0405@yahoo.co.jp>
2025-01-17 21:53:32 +0900

    削除機能ユーザーのみ設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,10 +35,14 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
+    if current_user.id == @item.user_id
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show, status: :unprocessable_entity
+      end
     else
-      render :show, status: :unprocessable_entity
+      redirect_to root_path
     end
   end
 
