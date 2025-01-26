6e7343b4730f67b80c9ae56d2af7b186afb9c12b
suzukozy <suzukozy0405@yahoo.co.jp>
2025-01-26 20:26:55 +0900

    レビュー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,16 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # 新規登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
                                         :nickname,
